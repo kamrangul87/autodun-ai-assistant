3e3db96e8de377899cc4e14897d1863a985b9e00
kamrangul87 <kamrangul87@gmail.com>
2025-12-24 08:55:36 +0000

    Update debug-env.ts

--- src/pages/api/debug-env.ts
+++ src/pages/api/debug-env.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Content-Type", "application/json");
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
-    EV_FINDER_NEARBY_URL: process.env.EV_FINDER_NEARBY_URL || null,
-    EV_FINDER_STATIONS_URL: process.env.EV_FINDER_STATIONS_URL || null,
-    MOT_PREDICTOR_API_URL: process.env.MOT_PREDICTOR_API_URL || null,
-    MOT_PREDICTOR_WEB_URL: process.env.MOT_PREDICTOR_WEB_URL || null,
+    EV_FINDER_STATIONS_URL: process.env.EV_FINDER_STATIONS_URL ?? null,
+    MOT_PREDICTOR_API_URL: process.env.MOT_PREDICTOR_API_URL ?? null,
+    MOT_PREDICTOR_WEB_URL: process.env.MOT_PREDICTOR_WEB_URL ?? null,
+    EV_FINDER_WEB_URL: process.env.EV_FINDER_WEB_URL ?? null,
   });
 }
